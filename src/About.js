d40c7f04779f256eb5285c936256521f1f03121b
Will Haller <40367144+wheelwastaken@users.noreply.github.com>
2022-03-15 16:53:24 -0400

    finally finished up about page

--- src/About.js
+++ src/About.js
@@ -13,39 +13,56 @@ function About(){
             <div className="content">
                 <h1>ABOUT WILL</h1>
 
+                <h2>PROFESSIONALLY</h2>
                 <p>
-                    Breakfast agreeable incommode departure it an. 
-                    By ignorant at on wondered relation. Enough at tastes really so cousin am of. Extensive therefore supported by extremity of contented. 
-                    Is pursuit compact demesne invited elderly be. View him she roof tell her case has sigh. Moreover is possible he admitted sociable concerns. 
-                    By in cold no less been sent hard hill.
+                    In the workplace, Will is a highly motivated, collaborative, and creative individual.  
+                    He graduated from Endicott College in May 2021 with a Bachelors Degree in Computer Science and a minor in Graphic Design.
+                    As of 2022, he has worked three great internships that were primarily focused on Front End Development, UI/UX Design, and 
+                    even 3D Printing.  Currently, he is looking for employment with a company focused on delivering a product or service in need of 
+                    a good UI/UX Engineer or Front End Engineer.  He plans to be a lead UI/UX Designer in the next five to ten years, and just needs to
+                    get his foot in the door to really prove himself.  
+                </p>
 
-                    Unpleasant astonished an diminution up partiality. Noisy an their of meant. 
-                    Death means up civil do an offer wound of. Called square an in afraid direct. 
-                    Resolution diminution conviction so mr at unpleasing simplicity no. 
-                    No it as breakfast up conveying earnestly immediate principle. Him son disposed produced humoured overcame she bachelor improved. 
-                    Studied however out wishing but inhabit fortune windows.
-
-                    Do so written as raising parlors spirits mr elderly. Made late in of high left hold. 
-                    Carried females of up highest calling. Limits marked led silent dining her she far. 
-                    Sir but elegance marriage dwelling likewise position old pleasure men. Dissimilar themselves simplicity no of contrasted as.
-                    Delay great day hours men. Stuff front to do allow to asked he.
-
-                    It sportsman earnestly ye preserved an on. Moment led family sooner cannot her window pulled any. 
-                    Or raillery if improved landlord to speaking hastened differed he. Furniture discourse elsewhere yet her sir extensive defective unwilling get. 
-                    Why resolution one motionless you him thoroughly. Noise is round to in it quick timed doors. Written address greatly get attacks inhabit pursuit our but. 
-                    Lasted hunted enough an up seeing in lively letter. Had judgment out opinions property the supplied.
-
-                    Maids table how learn drift but purse stand yet set. Music me house could among oh as their. 
-                    Piqued our sister shy nature almost his wicket. Hand dear so we hour to. He we be hastily offence effects he service. 
-                    Sympathize it projection ye insipidity celebrated my pianoforte indulgence. Point his truth put style. 
-                    Elegance exercise as laughing proposal mistaken if. We up precaution an it solicitude acceptance invitation. 
+                <h2>PERSONALLY</h2>
+                <p>
+                    Outside of the workplace, Will tries to keep himself busy focusing on other things that he loves.  In the collage on the left,
+                    you may see many different characters from video games, cartoons, comics, and much more.  Fighting Games are a big part of Will's life,
+                    being the main place he branches out in the community, through teaching, working on projects, or just having fun.  Will also does freelance work 
+                    for the local Fighting Game Community (FGC), in the forms of graphic design and video work.  On top of that, he is also working on his own games
+                    in Unity, building his own brand, and constantly working to avoid the dreaded nine to five eventually.  Below are his socials, feel free to connect
+                    with him on any of those platforms.
                 </p>
 
                 <ul className="buttons">
-                    <li><a href="/">IDK</a></li>
+                    <li><a href="./files/willHallerTraits.pdf" download="willHallerTraits">TRAITS</a></li>
                     <li><a href="./files/willHaller_resume.pdf" download="willHallerResume">RESUME</a></li>
                     <li><a href="/">HOME</a></li>
                 </ul>
+
+                <ul className="socials">
+                    <li>
+                        <a href="https://rateyourmusic.com/~_wheel_">
+                            <img src={"images/rym.png"} alt="The RateYourMusic Logo" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://letterboxd.com/wheel_/">
+                            <img src={"images/letterboxd-mac-icon.png"} alt="The Letterboxd Logo" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/wheelwastaken">
+                            <img src={"images/github.png"} alt="The GitHub Logo" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/will-haller/">
+                            <img src={"images/linkedin.png"} alt="The LinkedIn Logo" />
+                        </a>
+                    </li>
+                </ul>
+
+                
             </div>
             
         </div>
